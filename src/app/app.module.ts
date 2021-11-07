5bb64ed2cf493d84841b18a3111527507d9d184e
LEONEL ALBERTO THOMAS VILLALOBOS <leothomasv@unitec.edu>
2021-11-07 15:25:53 -0600

    Conectado a la base de datos Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';//firebase
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';//firebase
+import { provideDatabase,getDatabase } from '@angular/fire/database';//firebase
+import { provideStorage,getStorage } from '@angular/fire/storage';//firebase
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'fundacion-padrino',
+    appId: '1:974383338563:web:3593722f579851cf8c70f6',
+    storageBucket: 'fundacion-padrino.appspot.com',
+    apiKey: 'AIzaSyA0qUiPqOHEn5qwuuOBmX0n4KXx3NL_-5E',
+    authDomain: 'fundacion-padrino.firebaseapp.com',
+    messagingSenderId: '974383338563',
+    measurementId: 'G-1K8PD07ZGG',
+  },
   production: false
 };
 
