62fe61a8bc6bc5cd99cf307f4db3ee9f71b8bd00
nicole2112 <nicole.rodriguez@unitec.edu>
2021-11-07 14:57:10 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'pruebai18.component.html'
 })
 export class AppComponent {
   title = 'Proyecto-ISW';
