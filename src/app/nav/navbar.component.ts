7d719cd71588ad81070e5e3e5ac2f1ba58db1669
Aaron Merino <lopez.aaron1022@gmail.com>
2021-11-15 14:38:00 -0600

    Navbar Styling

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,8 +1,8 @@
 import { Component } from "@angular/core";
-
 @Component ({
     selector: 'navbar',
-    templateUrl: './navbar.component.html'
+    templateUrl: './navbar.component.html',
+    styleUrls: ['navbar.component.css']
 })
 
 export class NavbarComponent {
