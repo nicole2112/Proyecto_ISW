39473f152e52cb9c8155284639332d0db84ed1e5
Aaron Merino <lopez.aaron1022@gmail.com>
2021-11-18 09:20:57 -0600

    Missing Components

---
+++ src/app/donations/donations.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'donations',
+    templateUrl: './donations.component.html'
+})
+
+export class DonationsComponent{}
---
+++ src/app/testimonies/testimonies.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'testimonies',
+    templateUrl: './testimonies.component.html'
+})
+
+export class TestimoniesComponent{}
