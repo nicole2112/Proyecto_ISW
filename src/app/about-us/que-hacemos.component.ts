7b9e279cab0f6c604dbca1e94c8b3d5463aff003
Angella Falck <angella.falck@unitec.edu>
2021-11-22 03:34:26 -0600

    Cambios en testimonios, heroes y landing page

--- src/app/about-us/que-hacemos.component.ts
+++ src/app/about-us/que-hacemos.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component ({
-    templateUrl: './que-hacemos.component.html'
+    templateUrl: './que-hacemos.component.html',
 })
 
 export class QueHacemosComponent {
