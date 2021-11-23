f1b4424251142b2705280663a4f6be5d226e6986
Angella Falck <angella.falck@unitec.edu>
2021-11-23 10:22:10 -0600

    Quienes somos y cambios

--- src/app/about-us/quienes-somos.component.ts
+++ src/app/about-us/quienes-somos.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 
 @Component ({
-    templateUrl: './quienes-somos.component.html'
+    templateUrl: './quienes-somos.component.html',
+    styleUrls: ['quienes-somos.component.css']
 })
 
 export class QuienesSomosComponent {
