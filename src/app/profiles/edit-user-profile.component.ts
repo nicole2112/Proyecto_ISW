b8261f7cbc72d776537f937293c3dd4776c1edb7
Nicole Rodríguez <nicole.rodriguez@unitec.edu>
2021-11-29 09:54:13 -0600

    Validaciones user profile

--- src/app/profiles/edit-user-profile.component.ts
+++ src/app/profiles/edit-user-profile.component.ts
@@ -6,12 +6,20 @@ import { Component } from "@angular/core";
 })
 
 export class EditUserProfileComponent {
+    //Form de configuración perfil
     nombre: any;
     apellido: any;
     telefono: any;
     correo: any;
     direccion: any;
-    
+    namePattern = "^[a-zA-Z ]*$";
+    emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+    phonePattern = "^[0-9]{8}$";
+    //Form de modificar contraseña
+    actualPass: any;
+    newPass: any;
+    newPassVal: any;
+
     constructor(){}
 
 }
