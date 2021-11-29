b84d3e552d3cd35fb43264f3bc316c9eba056a9b
Aaron Merino <lopez.aaron1022@gmail.com>
2021-11-29 03:09:03 -0600

    Addition of navbar-digitador, added styling to users table

--- src/app/portal-admin/portal-admin.component.ts
+++ src/app/portal-admin/portal-admin.component.ts
@@ -2,7 +2,6 @@ import { Component, ElementRef, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/compat/database';
 import { User } from '../models/user';
-
 @Component({
   selector: 'app-portal-admin',
   templateUrl: './portal-admin.component.html',
@@ -22,7 +21,7 @@ export class PortalAdminComponent implements OnInit {
   }
 
   FundacionRef: AngularFireList<any>;
-  
+
   ngOnInit(): void {
     this.FundacionRef = this.db.list('usuarios');
 
