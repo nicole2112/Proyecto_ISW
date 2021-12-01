4e6b46125306a89d12da630620b124decb880fde
Luis <lefmejia@unitec.edu>
2021-12-01 15:42:19 -0600

    Se agregaron testimonios a la base de datos y se creo un servicio para acceder a ellos

--- src/app/testimonies/testimonies.component.ts
+++ src/app/testimonies/testimonies.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { DomSanitizer, SafeHtml, SafeResourceUrl } from "@angular/platform-browser";
+import { TestimonyService } from "../services/testimony.service";
 
 @Component({
     selector: 'testimonies',
@@ -6,4 +8,34 @@ import { Component } from "@angular/core";
     styleUrls: ['testimonies.component.css']
 })
 
-export class TestimoniesComponent{}
+export class TestimoniesComponent implements OnInit
+{
+    testimonyList : any[];
+    urlList : string[];
+
+    constructor(private testimService: TestimonyService, private _sanitizer: DomSanitizer)
+    {
+
+    }
+
+    ngOnInit(): void {
+        //console.log(this.testimService.getTestimonies());
+        this.testimService.getTestimonies().subscribe((item) => {
+            //console.log(item);
+            // let nuevo = item;
+            // nuevo.forEach(element => {
+            //     element.video_url = this.inputVideo(element.video_url);
+            // });
+            this.testimonyList = item;
+            console.log(this.testimonyList);
+
+        });
+
+    }
+
+    inputVideo(url:string):SafeResourceUrl{
+        console.log(this._sanitizer.bypassSecurityTrustResourceUrl(url));
+        return this._sanitizer.bypassSecurityTrustResourceUrl(url);
+      }
+
+}
