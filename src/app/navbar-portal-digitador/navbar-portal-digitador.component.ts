67f176766e555aead7f596aa0b7695b22a0b4454
Aaron Merino <lopez.aaron1022@gmail.com>
2021-12-02 13:46:22 -0600

    Sección de blog: poner una imagen en grande de la publicación en lugar del autor

--- src/app/navbar-portal-digitador/navbar-portal-digitador.component.ts
+++ src/app/navbar-portal-digitador/navbar-portal-digitador.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { AuthenticationService } from '../services/auth.services';
+import firebase from '@firebase/app-compat';
 import { faUserCircle, faDollyFlatbed, faHandHoldingUsd } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -12,8 +13,22 @@ export class NavbarPortalDigitadorComponent {
     constructor( public service: AuthenticationService, private eRef: ElementRef) { }
 
     ngOnInit(): void {
+      this.isLogged();
     }
     
+    isLogged(){
+      let userexp = '';
+      firebase.auth().onAuthStateChanged(function(user){
+        if(user){
+          document.getElementById('user-display').innerHTML = '<fa-icon class="fa icons" [icon]="faUserCircle"></fa-icon>' + user.email + ' ▼';
+        }else{
+          console.log('Error');
+        }
+      })
+      console.log(userexp);
+      return userexp;
+    }
+
     logOut(){
       this.service.logout();
     }
