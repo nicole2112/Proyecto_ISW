658dca8fb8208c2be59d8b9a052c77914c773c64
Angella Falck <angella.falck@unitec.edu>
2021-12-03 17:53:24 -0600

    cambios en testimonios

--- src/app/portal-admin/verTestimonios.component.ts
+++ src/app/portal-admin/verTestimonios.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { TestimonyService } from "../services/testimony.service";
 
 @Component ({
     selector: 'verTestimonios',
@@ -6,6 +8,33 @@ import { Component } from "@angular/core";
     styleUrls: ['verTestimonios.component.css']
 })
 
-export class VerTestimoniosComponent {
+export class VerTestimoniosComponent implements OnInit{
+    testimonyList : any[];
+    urlList : string[];
+
+    constructor(private testimService: TestimonyService, private _sanitizer: DomSanitizer)
+    {
+
+    }
+
+    ngOnInit(): void {
+        //console.log(this.testimService.getTestimonies());
+        this.testimService.getTestimonies().subscribe((item) => {
+            //console.log(item);
+            // let nuevo = item;
+            // nuevo.forEach(element => {
+            //     element.video_url = this.inputVideo(element.video_url);
+            // });
+            this.testimonyList = item;
+            console.log(this.testimonyList);
+
+        });
+
+    }
+
+    inputVideo(url:string):SafeResourceUrl{
+        console.log(this._sanitizer.bypassSecurityTrustResourceUrl(url));
+        return this._sanitizer.bypassSecurityTrustResourceUrl(url);
+      }
 
 }
