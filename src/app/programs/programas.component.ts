c0d941bd45a44bc44b7468977b00086f03520ed0
Leonel <leothomasv@gmail.com>
2022-02-02 19:59:01 -0600

    Funcionando Leer PDF

--- src/app/programs/programas.component.ts
+++ src/app/programs/programas.component.ts
@@ -1,12 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, Pipe, PipeTransform } from "@angular/core";
+import { DomSanitizer} from '@angular/platform-browser';
 import { ContactUsComponent } from "../contact-us/contact-us.component";
+import { AuthenticationService } from "../services/auth.services";
 @Component({
     selector: 'programas',
     templateUrl: './programas.component.html',
     styleUrls: ['programas.component.css']
 })
 
-export class ProgramasComponent {
 
+
+
+export class ProgramasComponent implements OnInit{
+    archivo:any;
+    pdfList: any;
+
+    constructor(private service: AuthenticationService){}
+
+    ngOnInit(){
+        this.service.db.list('PDFS').valueChanges().subscribe(
+            pdf =>{
+                this.pdfList = pdf;
+                console.log(this.pdfList);
+                console.log(pdf);
+            }
+        )
+    }
 
 }
