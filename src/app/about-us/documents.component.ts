936a5103994da3ca751208f33501bf98d9d641f1
Angella Falck <angella.falck@unitec.edu>
2022-02-21 10:20:32 -0600

    Added Accordion

--- src/app/about-us/documents.component.ts
+++ src/app/about-us/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "../services/auth.services";
 
 @Component({
     selector: 'documents-page',
@@ -7,5 +8,23 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class DocumentsComponent {
+
+    archivo:any;
+    pdfList: any;
+    openAccordion = [];
+
+    constructor(private service: AuthenticationService){}
+
+    ngOnInit(){
+        this.service.db.list('PDF-Descargables').valueChanges().subscribe(
+            pdf =>{
+                this.pdfList = pdf;
+                this.pdfList.sort((a,b) => (new Date(a.Fecha).getTime() - new Date(b.Fecha).getTime())); 
+                //console.log(this.pdfList);
+            }
+        )
+    }
+
+    
 
 }
