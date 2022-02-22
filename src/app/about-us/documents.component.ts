d0760c0e9a4462223024d806c0006b4db3b8f21f
Angella Falck <angella.falck@unitec.edu>
2022-02-21 23:06:11 -0600

    fecha modificado

--- src/app/about-us/documents.component.ts
+++ src/app/about-us/documents.component.ts
@@ -11,18 +11,36 @@ export class DocumentsComponent {
 
     archivo:any;
     pdfList: any;
+    pdfFecha=[];
     openAccordion = [];
+    
+    
 
     constructor(private service: AuthenticationService){}
 
-    ngOnInit(){
-        this.service.db.list('PDF-Descargables').valueChanges().subscribe(
-            pdf =>{
-                this.pdfList = pdf;
-                this.pdfList.sort((a,b) => (new Date(a.Fecha).getTime() - new Date(b.Fecha).getTime())); 
-                //console.log(this.pdfList);
-            }
-        )
+    ngOnInit(): void {
+        this.service.db.list('PDF-Descargables').valueChanges().subscribe((pdfs) => {
+            this.pdfList = pdfs;
+            
+            this.pdfList.forEach((pdfs) =>
+            {
+
+                var parts =pdfs.Fecha.split('/');
+                var mydate = new Date(parts[0], parts[1] - 1, parts[2]); 
+                pdfs.Fecha=mydate.toLocaleString('es-ES',{ month: 'long', day:'2-digit',year:'numeric' });
+                this.pdfFecha.push(pdfs);
+                    
+            });
+
+            this.pdfFecha.sort((a,b) => {
+                let dateA = new Date(b.Fecha), dateB = new Date(a.Fecha)
+            return +dateA - +dateB;; 
+            });
+
+        });
+
+        
+
     }
 
     
