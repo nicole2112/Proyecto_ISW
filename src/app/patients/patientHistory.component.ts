58967cb68eb3c190bb9c5a5bad2857b7f74e4788
Sergio Suazo <sergio.suazo96@outlook.com>
2022-03-08 22:01:55 -0600

    Historial Paciente y Agregar Solicitud

--- src/app/patients/patientHistory.component.ts
+++ src/app/patients/patientHistory.component.ts
@@ -3,6 +3,9 @@ import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
 import { take } from 'rxjs/operators';
 import Swal from 'sweetalert2';
 import { AuthenticationService } from '../services/auth.services';
+import { SolicitudesService } from "../services/solicitudes.service";
+import { PacientesService } from "../services/pacientes.service";
+
 @Component({
   selector: 'app-patient-history',
   templateUrl: './patientHistory.component.html',
@@ -10,6 +13,37 @@ import { AuthenticationService } from '../services/auth.services';
 })
 export class PatientHistoryComponent {
 
-    constructor(public service: AuthenticationService) {}
+
+    miPaciente: any;
+    type: any =0;
+    listaSolicitudes: any;
+    listaOrd: any;
+
+    constructor(public service: AuthenticationService,private solicitudservice: SolicitudesService, private pacienteService: PacientesService) {}
+
+    buscarPaciente(){
+      var id = (<HTMLInputElement>document.getElementById("num")).value;
+      
+      this.pacienteService.getPaciente(id).subscribe(paciente => {
+          this.miPaciente = paciente[0];
+          if(this.miPaciente == null)
+          {
+            this.type = 2;
+          }
+          else{
+            this.type = 1;
+
+          }  
+      });;
+      
+      this.solicitudservice.getSolicitudes(id).subscribe(records => {
+        this.listaSolicitudes = records.sort((a, b) => {
+            let dateA = new Date(b.fecha), dateB = new Date(a.fecha)
+            return +dateA - +dateB;
+        });
+        this.listaOrd = this.listaOrd;
+    });
+      
+  }
 
 }
