82870591330d7bdde59ee52f5744ac6735f2a442
Angella Falck <angella.falck@unitec.edu>
2022-03-22 21:44:35 -0600

    Cambios SP7

--- src/app/donations/donations.component.ts
+++ src/app/donations/donations.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+
+import { Component} from "@angular/core";
+import { ModalDismissReasons, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
     selector: 'donations',
@@ -6,4 +8,31 @@ import { Component } from "@angular/core";
     styleUrls: ['./donations.component.css']
 })
 
-export class DonationsComponent{}
+export class DonationsComponent{
+
+    constructor(private modalService: NgbModal) {}
+
+    closeResult: string;
+
+    goToUrl(): void {
+        window.open('https://www.gofundme.com/', "_blank");
+    }
+
+    open(content) {
+        this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+          console.log(`Closed with: ${result}`);
+        }, (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+      }
+
+      private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+          return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+          return 'by clicking on a backdrop';
+        } else {
+          return `with: ${reason}`;
+        }
+      }
+}
